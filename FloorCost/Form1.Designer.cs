fc6b54998104319a4942504788f2c9ee9ed7b002
Benjamin Goff <bgoff@mainerobotics.org>
2019-10-18 22:06:39 -0400

    Add project files.

---
+++ FloorCost/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FloorCost
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // calc
+            // 
+            this.calc.Location = new System.Drawing.Point(408, 147);
+            this.calc.Name = "calc";
+            this.calc.Size = new System.Drawing.Size(75, 23);
+            this.calc.TabIndex = 0;
+            this.calc.Text = "button1";
+            this.calc.UseVisualStyleBackColor = true;
+            this.calc.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.calc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calc;
+    }
+}
+
